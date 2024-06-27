e1b3e4b84f0a5b747e80bfb12a0c93eb942852a5
DileepaE <145334712+DileepaE@users.noreply.github.com>
2024-06-27 13:57:22 +0100

    Workshop 27/06/2024

---
+++ codes/script.R
@@ -0,0 +1,26 @@
+#identify your working directory 
+getwd()
+
+#import data
+data <- read.csv("./data/data.csv")
+head(data)
+dim(data)
+
+
+#summary measures------ 
+mean(data$age)
+median(data$age)
+min(data$age)
+max(data$age)
+summary(data)
+
+#plots-------
+hist(data$age)
+boxplot(data$age)
+plot(data$age, data$bmi)
+
+##manipulate plots-------
+boxplot(data$age, col="lightblue", ylab="Years", main="Boxplot of Age")
+
+
+#hypothesis testing-----
